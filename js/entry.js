a220dd65b961ee5479a84eddecc84924df4e647c
Gil Greenberg <gilgreenberg@gmail.com>
2016-06-05 17:09:43 -0400

    Add new JS logic

--- js/entry.js
+++ js/entry.js
@@ -1,44 +1,35 @@
-require('./jquery.simpleWeather.min');
-
-// Docs at http://simpleweatherjs.com
 $(document).ready(function() {
-  $.simpleWeather({
-    woeid: '',
-    location: 'New York City',
-    unit: 'f',
-    success: function(weather) {
-      //console.log(weather);
-      var thunder = [38, 39, 47, 45, 3, 4];
-      var drizzle = [9];
-      var snow = [13, 13, 14, 15, 16, 41, 46, 42];
-      var rain = [11, 12, 40];
-      var hail = [17];
-      var sleet = [18];
+  var brandColors = new Array();
+  brandColors['instagram'] = '88,81,219,.5';
+  brandColors['twitter'] = '85,172,238,.5';
+  brandColors['github'] = '102,102,102,.5';
+  brandColors['linkedin'] = '0,119,181,.5';
 
-      if(drizzle.indexOf(weather.code) > -1) {
-        console.log('drizzle');
-        //$('body').animate({backgroundColor: '#F7AC57'}, 1500);
-      } else if(rain.indexOf(weather.code) > -1) {
-        console.log('rain');
-      } else if(thunder.indexOf(weather.code) > -1) {
-        console.log('thunder');
-      } else if(snow.indexOf(weather.code) > -1 || sleet.indexOf(weather.code) > -1 || hail.indexOf(weather.code) > -1) {
-        snowStorm.toggleSnow();
-      } else {
-        $('body').animate({backgroundColor: '#0091c2'}, 1500);
-      }
-      html = '<h1 class="icon-'+weather.code+'"></h1>';
-      html += '<div class="weather__details"><h2 class="weather__temp"><span class="temp__f">'+weather.temp+ '</span><span class="temp__c">'+weather.alt.temp+'</span>&deg;</h2>';
-      html += '<ul><li class="currently">'+weather.currently+'</li></ul></div>';
-      html += '<div class="weather__city">'+weather.city+', '+weather.region+'</div>';
-
-      //var timestamp = moment(weather.updated);
-      //html += '<p class="updated">Updated '+moment(timestamp).fromNow()+'</p>';
-
-      $("#weather").html(html);
-    },
-    error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
-    }
+  $(document).on('mouseover', '.section__intro .social-links a', function() {
+  	var brand = $(this).data('brand');
+  	var brandColor = brandColors[brand];
+  	$(this).closest('.section__intro').css('background-color', 'rgba(' + brandColor + ')');
+  }).on('mouseout', '.section__intro .social-links a', function() {
+  	$(this).closest('.section__intro').css('background-color', 'transparent');
   });
+
+  $('.about__words').each(function(){
+         // get current ul
+         var $ul = $(this);
+         // get array of list items in current ul
+         var $liArr = $ul.children('li');
+         // sort array of list items in current ul randomly
+         $liArr.sort(function(a,b){
+               // Get a random number between 0 and 10
+               var temp = parseInt( Math.random()*10 );
+               // Get 1 or 0, whether temp is odd or even
+               var isOddOrEven = temp%2;
+               // Get +1 or -1, whether temp greater or smaller than 5
+               var isPosOrNeg = temp>5 ? 1 : -1;
+               // Return -1, 0, or +1
+               return( isOddOrEven*isPosOrNeg );
+         })
+         // append list items to ul
+         .appendTo($ul);
+   });
 });
