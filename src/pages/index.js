faa5208e6a491b0c564498b20e50ef891be8bd83
Gil Greenberg <3484527+gil--@users.noreply.github.com>
2022-11-15 22:48:44 -0500

    Remove image from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,19 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Gil Greenberg</h1>
-    <p>Hey! I'm based in NYC and work full-time on <a href="https://www.checkoutpromotions.com" target="_blank">Checkout Promotions</a>, a Shopify app to grow customer life time value through the use of promotional offers during checkout.</p>
+    <p>Hey! I'm based in NYC and work full-time on <a href="https://apps.shopify.com/checkout-blocks" target="_blank">Checkout Blocks</a>, a Shopify Plus app to customize checkout.</p>
     <p>Previously, I was a Front End Technical Lead at <a href="//rightpoint.com" target="_blank">Rightpoint</a>.</p>
     <br />
     <p>In NYC? Reach out on social media and I'd be happy to grab a coffee (I respond best on Twitter).</p>
     <h2>Find me on...</h2>
     <ul>
       <li><a href="//www.twitter.com/gilgnyc" target="_blank">Twitter</a></li>
-      <li><a href="//www.instagram.com/gil.nyc/" target="_blank">Instagram</a></li>
       <li><a href="//www.linkedin.com/in/gilgreenberg" target="_blank">LinkedIn</a></li>
       <li><a href="//github.com/gil--" target="_blank">Github</a></li>
     </ul>
